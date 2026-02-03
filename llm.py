1330480d85341d7a7e43dc4c459f41e1e50e2cc5
heyysvm <imshivam339@gmail.com>
2026-02-03 16:07:33 +0530

    .

--- llm.py
+++ llm.py
@@ -1,13 +1,30 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 from google import genai
-client = genai.Client(api_key="AIzaSyDF5aBwrkhZfut4jWa1geQUPEE2yuaRSkg")
+import os
+
+client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
 def study_assistant(user_prompt):
-
     response = client.models.generate_content(
         model="gemini-2.5-flash",
-        contents = user_prompt
+        contents=user_prompt
     )
-    return response.text 
+    return response.text
 
-data = study_assistant("what is STRANGER THINGS")
-print(data)
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/ask", methods=["POST"])
+def ask_api():
+    data = request.get_json()
+    question = data.get("question")
+
+    if not question:
+        return jsonify({"error": "No question provided"}), 400
+
+    answer = study_assistant(question)
+    return jsonify({"answer": answer})
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8000)
